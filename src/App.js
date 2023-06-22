0fdc72b0b33ec77aa31b8232c407b17cf29b344e
SIRICHAI-P\CDG-PM2 <lazypuzzle@hotmail.com>
2023-06-22 15:37:59 +0700

    update ui react app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className='App-logo' alt='logo' />
         <div style={{ fontSize: 30 }}>
           Version{' '}
-          <span style={{ color: 'red' }}>Third and FINAL Commit....!!!</span>
+          <span style={{ color: 'green' }}>This is new Version aaagsdasgdasdgasdgasd....!!!</span>
         </div>
       </header>
     </div>
